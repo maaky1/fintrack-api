4ad0e105733c0df72d0f4a86cf4d67b7ca6cc93f
F <f.idevs@pm.me>
2026-02-12 16:49:50 +0700

    Add create table user migration

---
+++ internal/database/migrations/002_create_user_table.go
@@ -0,0 +1,27 @@
+package migrations
+
+import (
+	"context"
+
+	"github.com/uptrace/bun"
+)
+
+func init() {
+	Migrations.MustRegister(func(ctx context.Context, db *bun.DB) error {
+		_, err := db.ExecContext(ctx, `
+			CREATE TABLE IF NOT EXISTS "users" (
+				"id" BIGSERIAL PRIMARY KEY,
+				"clerk_user_id" TEXT UNIQUE NOT NULL,
+				"fullname" TEXT,
+				"created_at" TIMESTAMPTZ NOT NULL DEFAULT (now()),
+				"updated_at" TIMESTAMPTZ NOT NULL DEFAULT (now())
+			);
+		`)
+		return err
+	}, func(ctx context.Context, db *bun.DB) error {
+		_, err := db.ExecContext(ctx, `
+			DROP TABLE IF EXISTS "users";
+		`)
+		return err
+	})
+}
